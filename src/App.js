1661e5d6f0135c509b8fb08242e13e6878f86e5e
patrick <po44243@gmail.com.com>
2024-11-24 20:47:42 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {Navbar,NavList} from "./components/navbar";
+// import NavList from "./components/navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Navbar >
+        <NavList>
+          <li>Primeiro Item</li>
+          <li>Segundo Item</li>
+          <li>Terceiro Item</li>
+        </NavList>
+      </Navbar>
+    
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Navbar = styled.nav`
+display: flex;
+background-color: black;
+color: white;
+padding: 10px;
+font-size: 20px;
+justify-content: center;
+align-items: center;
+`;
+
+export const NavList = styled.ul`
+display: flex;
+list-style-type: none;
+margin: 0;
+padding: 0;
+gap: 40px;
+
+`;
+
+// export default {Navbar,NavList}
